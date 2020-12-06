597568b26304ff790238b238f66a00310878dd6d
elvisPony <elvisPony@users.noreply.github.com>
2020-12-06 23:53:09 +0800

    Update serve.py

--- chal/bin/serve.py
+++ chal/bin/serve.py
@@ -1,4 +1,39 @@
-#!/usr/bin/env python3
+import socket
+from random import randint
 
-a = input("TEST")
-print(a)
+# SOCKET START
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(( "0.0.0.0", 4000))
+s.listen(60)
+#print('socket start')
+# SOCKET END
+
+flag = "flag{SoRting_}"
+
+def dosort():
+    arr = [randint(1, 100) for _ in range(6)]
+    return arr, sorted(arr)
+
+errormessage ="Error"
+
+while True:
+    conn, addr = s.accept()
+    try:
+        for _ in range(1000):
+            q, a = dosort()
+            ans = '['+', '.join([str(i) for i in a])+']'
+            #print(ans)
+            conn.sendall((' '.join([str(i) for i in q]) + '\n').encode())
+            conn.sendall("Answer: ".encode())
+            data = conn.recv(1024).strip().decode()
+            #print(data)
+            if (data != ans):
+                conn.sendall(errormessage.encode())
+                conn.close()
+
+        conn.sendall(flag.encode())
+        conn.close()
+    except socket.error:
+        print("ERROR")
+        conn.close()
